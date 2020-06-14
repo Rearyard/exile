767138aabf5db2f42c60ceeeaefd7b73f4774ca7
RikaSugisawa <tjj.rikap@gmail.com>
2020-06-14 20:39:24 +0800

    系统通知和点赞

---
+++ src/views/messages/Index.vue
@@ -0,0 +1,69 @@
+<!--eslint-disable-->
+<!--suppress ALL -->
+<template>
+  <div>
+    <List>
+      <ListItem v-for="r in rows"
+                @click.native="$router.push('/message/'+r.type)"
+                style="display: flex;padding:1em;background-color:#fff;cursor: pointer">
+        <Icon :type="r.icon" color="white" :size="40"/>
+        <span style="margin: 1em">{{r.title}}</span>
+        <span class="cnt-info">{{r.cnt>0?r.cnt:''}}</span>
+        <div class="flex-space"/>
+        <Icon type="ios-arrow-forward" :size="16"/>
+      </ListItem>
+    </List>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from "vue-property-decorator";
+
+  @Component
+  export default class MessageIndex extends Vue {
+    rows = [
+      {type: 'notification', cnt: 2147483647, title: '系统通知', icon: 'ios-notifications-outline'},
+      {type: 'directMsg', cnt: 3, title: '私信', icon: 'ios-mail-outline'},
+      {type: 'comment', cnt: 0, title: '评论', icon: 'ios-chatboxes-outline'},
+      {type: 'like', cnt: 12, title: '赞', icon: 'ios-thumbs-up-outline'},
+    ]
+
+    mounted() {
+    }
+  }
+</script>
+
+<style>
+  .ivu-icon-ios-notifications-outline {
+    background: lightgray;
+  }
+
+  .ivu-icon-ios-mail-outline {
+    background: lightblue;
+  }
+
+  .ivu-icon-ios-chatboxes-outline {
+    background: #acdeac;
+  }
+
+  .ivu-icon-ios-thumbs-up-outline {
+    background: pink;
+  }
+
+  .ivu-list-item .ivu-icon {
+    padding: 4px;
+    border-radius: 50%;
+  }
+
+  .flex-space {
+    flex-grow: 999;
+  }
+
+  .cnt-info {
+    border-radius: 1em 1em;
+    background: rgba(255, 0, 0, 0.6);
+    padding: 0 .5em;
+    color: white;
+    font-weight: bold;
+  }
+</style>
---
+++ src/views/messages/Notification.vue
@@ -0,0 +1,42 @@
+<!--eslint-disable-->
+<!--suppress ALL -->
+<template>
+  <List>
+    <ListItem class="ul-row" v-for="r in notifications">
+      <div class="content">{{r.content}}
+      </div>
+      <div class="times-ago">{{moment(r.time).fromNow()}}</div>
+    </ListItem>
+  </List>
+</template>
+<script lang="ts">
+  /* eslint-disable */
+  import {Component, Vue} from "vue-property-decorator";
+  import moment from "moment";
+
+  @Component
+  export default class MessageNotification extends Vue {
+    moment = moment
+    notifications = new Array(10).fill({
+      content: '系统消息：尊敬的后花园用户，您好！根据相关法律法规和社区管理规定嘟噜噜噜噜噜。',
+      time: "2020-06-03T12:32:54.000Z"
+    })
+  }
+</script>
+
+<style lang="less">
+  .ul-row {
+    background: white;
+    padding: 1em !important;
+
+    & > .content {
+    }
+
+    & > .times-ago {
+      display: block;
+      width: 100%;
+      font-size: small;
+      text-align: right;
+    }
+  }
+</style>
