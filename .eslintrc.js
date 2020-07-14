11ccb9d5b6a3f5b34e80032115e68862d312a3e9
CafuChino <kirino@cafuchino.cn>
2020-07-14 18:44:25 +0800

    完成密码重置部分

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    '@typescript-eslint/camelcase':'off'
+    '@typescript-eslint/camelcase':'off',
+    'no-undef':'off'
   }
 }
